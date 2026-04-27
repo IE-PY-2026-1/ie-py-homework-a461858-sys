083bc2af5443a03a58dcde68e28260b1c0c5edbc
a461858-sys <a461858@mju.ac.kr>
2026-04-28 01:40:20 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 # 파일이름 : 2차과제
 # 작 성 자 : 60251785 이수연
 
-todo_list = []
+tasks =[]
+categories =[]
+priorities = []
+
 
 print('투두리스트 입력')
-task = input('투두리스트 입력: ')
-category = input('목록(공부/취미/자기개발/기타): ')
-priority = int(input('중요도(1-3): '))
+t_input = input('투두리스트 입력: ')
+c_input = input('목록(공부/취미/자기개발/기타): ')
+p_input = int(input('중요도(1-3): '))
 
-todo_list.append([task, category, priority])
-print(f'{task}가 저장되었습니다!')
+tasks.append(t_input)
+categories.append(c_input)
+priorities.append(p_input)
+print(f'{t_input}가 저장되었습니다!')
 
 Print(f'----항목보기메뉴----')
 print('1. 전체 목록')
@@ -20,14 +25,19 @@ choice = input('보고 싶은 목록 번호를 입력하세요.: ')
 
 if choice == '1':
   print(f'[전체 목록 조회]')
-  print(f'할 일: {todo_list=})
+  print(f'할 일: {tasks[0]} / 카테고리: {categories} / 중요도: {priorities})
                 
 elif choice == '2':
   print('f'[중요도 별 목록보기]')
-  select_priority = int(input('조회할 중요도(1-3)'))
+  select_p = int(input('조회할 중요도(1-3)'))
   print(f'[중요도 {select_priority} 목록 결과];)
-  if todo_list[0][2] == selsect_priority:
-     print(f'{todo_list[0][0]} (카테고리: {todo_list[0][1]})')
+  if priorities[0] == select_p:
+     if select == 3:
+        print('매우 긴급!!!')
+     elif select == 2:
+        print('보통')
+     else:
+        print('여유~')
    else:
      print(f'해당 중요도({select_priority}) 와 일치하는 할 일이 없습니다.')
 
@@ -35,8 +45,9 @@ elif choice == '2':
 elif choice == '3':
   search_cat = input('조회할 카테고리(공부/취미/자기개발/기타): ')
   print(f'[{select_cat} 카테고리 조회 결과]')
-  if todo_list[0][1] == search_cat:
-     print(f'{todo_list[0][0]} (중요도 : {todo_list[0][2]})')
+  
+  if categories[0] == search_cat:
+     print(f'{tasks[0]} (중요도 : {priorities[0]})')
    else:
      print(f'{search_cat} 카테고리에 해당하는 할 일이 없습니다.')
 else:
